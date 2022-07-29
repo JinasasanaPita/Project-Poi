55b58bbf08d50a9ebf500ead5621766e5fd5fb9b
JinasasanaPita <46955258+JinasasanaPita@users.noreply.github.com>
2022-07-29 12:09:55 +0800

    Added mechanics to feature mechanics branch
    Added camera script and tilemap to feature mechanics testing

---
+++ Project Poi/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float cameraSpeed = 5.0f;
+    public float cameraDragSpeed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translateX = Input.GetAxis("Horizontal");
+        float translateZ = Input.GetAxis("Vertical");
+        Vector3 translation = new Vector3(translateX, 0, translateZ) * Time.deltaTime * cameraSpeed;
+
+        this.transform.Translate(translation, Space.World);
+
+        if (Input.GetMouseButton(0))
+        {
+            translateX = Input.GetAxis("Mouse X");
+            translateZ = Input.GetAxis("Mouse Y");
+            translation = new Vector3(-translateX, 0, -translateZ) * Time.deltaTime * cameraDragSpeed;
+
+            this.transform.Translate(translation, Space.World);
+        }
+    }
+}
---
+++ Project Poi/Assets/Scripts/PrefabBrush.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Prefab brush", menuName = "Brushes/Prefab brush")]
+[CustomGridBrush(false, true, false, "Prefab Brush")]
+public class PrefabBrush : GameObjectBrush
+{
+      
+}
