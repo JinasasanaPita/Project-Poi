c657846f7808a20a8ca4a1859df57d3863325ed4
JinasasanaPita <46955258+JinasasanaPita@users.noreply.github.com>
2022-08-18 20:49:51 +0800

    Fixed camera
    Fixed camera such that camera travels on its own space without losing height

--- Project Poi/Assets/Scripts/CameraController.cs
+++ Project Poi/Assets/Scripts/CameraController.cs
@@ -7,10 +7,13 @@ public class CameraController : MonoBehaviour
     public float cameraSpeed = 5.0f;
     public float cameraDragSpeed = 10.0f;
     public float cameraRotateSpeed = 10.0f;
+
+    public float cameraHeight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraHeight = Camera.main.transform.position.y;
     }
 
     // Update is called once per frame
@@ -19,16 +22,22 @@ public class CameraController : MonoBehaviour
         float translateX = Input.GetAxis("Horizontal");
         float translateZ = Input.GetAxis("Vertical");
         Vector3 translation = new Vector3(translateX, 0, translateZ) * Time.deltaTime * cameraSpeed;
+        this.transform.Translate(translation, Space.Self);
 
-        this.transform.Translate(translation, Space.World);
+        float dHeight = cameraHeight - this.gameObject.transform.position.y;
+        Vector3 translateY = new Vector3(0, dHeight, 0);
+        this.transform.Translate(translateY, Space.World);
 
         if (Input.GetMouseButton(0))
         {
             translateX = Input.GetAxis("Mouse X");
             translateZ = Input.GetAxis("Mouse Y");
-            translation = new Vector3(-translateX, 0, -translateZ) * Time.deltaTime * cameraDragSpeed;
+            translation = new Vector3(-translateX, 0, -translateZ) * Time.deltaTime * cameraSpeed;
+            this.transform.Translate(translation, Space.Self);
 
-            this.transform.Translate(translation, Space.World);
+            dHeight = cameraHeight - this.gameObject.transform.position.y;
+            translateY = new Vector3(0, dHeight, 0);
+            this.transform.Translate(translateY, Space.World);
         }
 
         if (Input.GetMouseButton(1))
